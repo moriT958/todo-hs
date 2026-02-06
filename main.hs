b3090274e200fb01408edd04cb5a56c28566c5ab
morita <morit958@gmail.com>
2026-02-06 23:40:42 +0900

    なんとなくリファクタ

--- main.hs
+++ main.hs
@@ -1,48 +1,59 @@
-import Data.List (delete)
 import System.Environment (getArgs)
-import System.IO
 
 -- タスクを保存するファイル
 todoFile :: FilePath
 todoFile = "store.txt"
 
--- MAIN 関数
-main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    ["add", task] -> addTask task
-    ["list"] -> listTasks
-    ["done", num] -> doneTask (read num)
-    _ -> usage
+formatTasks :: [String] -> [String]
+formatTasks = zipWith (\n t -> show n ++ ": " ++ t) [1 :: Int ..]
+
+removeTaskAt :: Int -> [String] -> Maybe ([String], String)
+removeTaskAt n tasks
+  | n < 1 || n > length tasks = Nothing
+  | otherwise = Just (take (n - 1) tasks ++ drop n tasks, tasks !! (n - 1))
+
+parseTaskNumber :: String -> Maybe Int
+parseTaskNumber s = case reads s of
+  [(n, "")] | n > 0 -> Just n
+  _ -> Nothing
 
 usage :: IO ()
-usage = do
-  putStrLn "Usage:"
-  putStrLn "  todo add \"<task>\""
-  putStrLn "  todo list"
-  putStrLn "  todo done <id>"
+usage =
+  mapM_
+    putStrLn
+    [ "Usage:",
+      "  todo add \"<task>\"",
+      "  todo list",
+      "  todo done <id>"
+    ]
 
 -- タスクを追加
 addTask :: String -> IO ()
-addTask task = do
+addTask task =
   appendFile todoFile (task ++ "\n")
-  putStrLn "Added a new task!"
+    >> putStrLn "Added a new task!"
 
 -- タスク一覧表示
 listTasks :: IO ()
-listTasks = do
-  contents <- readFile todoFile
-  let tasks = lines contents
-  mapM_ putStrLn $
-    zipWith (\i t -> show i ++ ": " ++ t) [1 :: Int ..] tasks
+listTasks =
+  readFile todoFile
+    >>= mapM_ putStrLn . formatTasks . lines
 
 -- タスク完了 (削除)
 doneTask :: Int -> IO ()
 doneTask n = do
-  contents <- readFile todoFile
-  let tasks = lines contents
-      task = tasks !! (n - 1)
-      newTasks = delete task tasks
-  length contents `seq` writeFile todoFile (unlines newTasks)
-  putStrLn ("Done: " ++ task)
+  tasks <- lines <$> readFile todoFile
+  case removeTaskAt n tasks of
+    Nothing -> putStrLn $ "Error: Invalid task number " ++ show n
+    Just (newTasks, removed) ->
+      writeFile todoFile (unlines newTasks)
+        >> putStrLn ("Done: " ++ removed)
+
+main :: IO ()
+main = getArgs >>= dispatch
+  where
+    dispatch ["add", task] = addTask task
+    dispatch ["list"] = listTasks
+    dispatch ["done", num] = maybe invalidNum doneTask (parseTaskNumber num)
+    dispatch _ = usage
+    invalidNum = putStrLn "Error: Please provide a valid task number"
